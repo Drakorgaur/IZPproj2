4266af5fd5b4d3e93e43d5b7b04b777474f27c8c
Mark Ermine <re.markofdark@gmail.com>
2021-11-24 12:46:43 +0100

    main_test.c is working 2d dynamic array

--- main_test.c
+++ main_test.c
@@ -24,18 +24,18 @@ void upgrade(Array *A)
     A->array = (char **)realloc(A->array, sizeof(char **)*A->row_size);
 
     for(int i=0; i<A->row_used; ++i){
-        A->array[i] = (char *)realloc(A->array[i], sizeof(char)*A->row_size);
+        A->array[i] = (char *)realloc(A->array[i], sizeof(char)*A->column_size);
     }
 
     for(size_t i=A->row_used; i<A->row_size; ++i){
-        *(A->array+i) = (char *)malloc(sizeof(char)*A->row_size);
+        *(A->array+i) = (char *)malloc(sizeof(char)*A->column_size);
     }
 
 }
 
 int main(int argc, char const *argv[]) {
 
-    int N = 10, M = 15, count = 0;
+    int N = 10;
     Array A;
 
     initArray(&A, N);
@@ -47,32 +47,43 @@ int main(int argc, char const *argv[]) {
     }
 
     for(int i=0; i<A.row_size; ++i){
+        printf("%d", i);
         for(int j=0; j<A.row_size; ++j){
             printf("%c ", A.array[i][j]);
         }
         printf("\n");
     }
     A.row_size += 3;
+    A.column_size += 1;
 
     upgrade(&A);
 
-    for(size_t i=0; i<A.row_used; ++i){
-        for(size_t j=A.row_used; j<A.row_size; ++j){
-            A.array[i][j] = 'b';
-        }
-    }
-    for(size_t i=A.row_used; i<A.row_size; ++i){
-        for(size_t j=0; j<A.row_size; ++j){
+    for(int i=0; i<A.row_size; ++i){
+        printf("%d", i);
+        for(int j=0; j<A.column_size; ++j){
             A.array[i][j] = 'c';
-        }
-    }
-
-    for(size_t i=0; i<A.row_size; ++i){
-        for(size_t j=0; j<A.row_size; ++j){
             printf("%c ", A.array[i][j]);
         }
         printf("\n");
     }
+
+//    for(size_t i=0; i<A.row_used; ++i){
+//        for(size_t j=A.row_used; j<A.row_size; ++j){
+//            A.array[i][j] = 'b';
+//        }
+//    }
+//    for(size_t i=A.row_used; i<A.row_size; ++i){
+//        for(size_t j=0; j<A.row_size; ++j){
+//            A.array[i][j] = 'c';
+//        }
+//    }
+//
+//    for(size_t i=0; i<A.row_size; ++i){
+//        for(size_t j=0; j<A.row_size; ++j){
+//            printf("%c ", A.array[i][j]);
+//        }
+//        printf("\n");
+//    }
 
     return 0;
 }
