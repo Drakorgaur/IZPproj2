280f9a57b1fe0100c8b7ace5be2fb129683408ee
Mark Ermine <re.markofdark@gmail.com>
2021-11-30 12:20:51 +0100

    small fixes of leaking #1.1.8 [MEMORY-1-SUCCESS]

--- mainV2.c
+++ mainV2.c
@@ -629,7 +629,7 @@ int main(int argc, char **argv) {
     (void)argc;
     Memory *memory = createMemory();
 //    result* res = createResult();
-//    readFromFileV2(argv[1], memory);
+    readFromFileV2(argv[1], memory);
 
 //    if (!checkForRelationAndSetElementsInUniversum(memory)) {
 //        printf("ERROR: relation is not valid\n");
